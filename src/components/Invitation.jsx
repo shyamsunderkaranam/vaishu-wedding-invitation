8ef0e381b9dfd1ce4e9bb64103fa329942cba172
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2024-10-31 05:51:42 +0530

    some animation fixes

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -13,7 +13,8 @@ const Invitation = () => {
   // const lastScrollY = useRef(0);
   
   useEffect(() => {
-    AOS.init();
+    AOS.init({ once: true, offset: 200, duration: 1000, easing: 'ease-out-cubic' }); 
+    AOS.refresh();
     const leftCard = document.querySelector('.left-card');
     const rightCard = document.querySelector('.right-card');
     
@@ -22,50 +23,6 @@ const Invitation = () => {
       rightCard.style.transform = 'translateX(0)';
     }, 500);
 
-    // const handleScroll = () => {
-    //   const currentScrollY = window.scrollY;
-    //   if (currentScrollY > lastScrollY.current) {
-    //     leftCard.classList.add('disappear');
-    //     rightCard.classList.add('disappear');
-    //   } else {
-    //     leftCard.classList.remove('disappear');
-    //     rightCard.classList.remove('disappear');
-    //   }
-    //   lastScrollY.current = currentScrollY;
-    // };
-
-    // window.addEventListener('scroll', handleScroll);
-
-    // return () => {
-    //   window.removeEventListener('scroll', handleScroll);
-    // };
-    /*
-    const observerOptions = {
-      root: null,
-      threshold: 0.1,
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          leftCard.style.transform = 'translateX(0)';
-          rightCard.style.transform = 'translateX(0)';
-        } else {
-          leftCard.style.transform = 'translateX(-100vw)';
-          rightCard.style.transform = 'translateX(100vw)';
-        }
-      });
-    }, observerOptions);
-
-    if (mainCardsRef.current) {
-      observer.observe(mainCardsRef.current);
-    }
-
-    return () => {
-      if (mainCardsRef.current) {
-        observer.unobserve(mainCardsRef.current);
-      }
-    };*/
     setTimeout(()=>{
       confetti({
       particleCount: 100,
@@ -76,14 +33,6 @@ const Invitation = () => {
 
   }, []);
 
-  // const bride = { name: "VAISHNAVI", img: "vaishu.jpg" };
-  // const groom = { name: "KARTHIK", img: "karthik.jpg" };
-  // const venue = { title: "Sri Siddi Vinayaka Temple", 
-  //   details: "Rejinthal Village,Zaheerabad, Mirzapur[B], Telangana 502249", 
-  //   map: "https://maps.app.goo.gl/Vwajr2koTKJmPEmV7"
-  // };
-  // const date = { title: 'Date & Time:', details: "8th December 2024 and Muhurtham time" };
-  // const invitees = ["KULKARNI'S FAMILY"]; // Add more invitees as needed
   const {bride, groom, venue, date, invitees} = wedding;
   return (
     <div className="App">
@@ -112,19 +61,17 @@ const Invitation = () => {
           
           <div className="card detail-card"
           data-aos="flip-left"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000"
+          
           >
             <h3>Venue:</h3>
             <p>{venue.title}</p>
             <p>{venue.details}</p>
-            <a href={venue.map} target="_blank" rel="noreferrer">View on Map</a>
+            <a class="btn" href={venue.map} target="_blank" rel="noreferrer">View on Map</a>
           </div>
           
           <div className="card detail-card"
           data-aos="flip-right"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000"
+          
           >
             <h3>{date.title}</h3>
             <p>{date.details}</p>
